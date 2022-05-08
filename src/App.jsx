fcd9f730d3fe60aee260924fa577a7881c63fc98
Jason Wan <jaysun1w@gmail.com>
2022-05-08 13:47:57 -0500

    Base files

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React,{Component,Fragment} from 'react'
+import CardList from '../CardList'
+import Searchbar from '../Searchbar'
+import './App.css'
+import Scroll from '../Scroll'
+
+
+class App extends Component {
+
+  constructor(){
+  super()
+  this.state = {
+  robots : [],
+  searchField: ''
+  }
+  }
+
+  
+  onSearchChange = (e) => {
+    this.setState({searchField: e.target.value})
+    
+  }
+
+  componentDidMount(){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=>{
+        return(response.json())
+      })
+      .then(users=>{
+        this.setState({robots: users})
+      });
+
+  }
+  render(){
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+    if (this.state.robots.length == 0){
+      return <h1>Loading...</h1>
+    }
+    else{
+
+      return (
+        <>
+    <div className = "tc">
+    <h1>Robo Friends</h1>
+    <Searchbar searchChange = {this.onSearchChange}/>
+    <Scroll>
+    <CardList robots = {filteredRobots}/>
+    </Scroll>
+    </div>
+    </>
+  )
+}
+}
+  
+}
+
+export default App;
